5b72042ab8a6e96bcd59111d878366752f392983
GastonMora42 <gastonmora1742@gmail.com>
2024-04-02 13:21:07 -0300

    Probamos otra manera de leer las credenciales google

--- get_last_file.py
+++ get_last_file.py
@@ -6,29 +6,37 @@ import json
 
 def configure_google_drive():
     gauth = GoogleAuth()
-    
-    # Obtener el JSON de las credenciales desde la variable de entorno
-    credentials_json = os.getenv('MYCREDSGOOGLE')
-    
-    if credentials_json is None:
-        raise ValueError("No se encontró el JSON de credenciales en la variable de entorno")
 
-    # Cargar las credenciales desde el JSON
-    credentials = json.loads(credentials_json)
-    
+    # Obtener las credenciales desde variables de entorno
+    client_id = os.getenv('CLIENT_ID')
+    client_secret = os.getenv('CLIENT_SECRET')
+    refresh_token = os.getenv('REFRESH_TOKEN')
+
     # Configurar las credenciales en gauth
-    gauth.credentials = credentials
+    gauth.credentials = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "refresh_token": refresh_token,
+        "token_expiry": None,
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "user_agent": None,
+        "revoke_uri": "https://oauth2.googleapis.com/revoke",
+        "scopes": ["https://www.googleapis.com/auth/drive"],
+        "_class": "OAuth2Credentials",
+        "_module": "oauth2client.client"
+    }
 
     # Verificar si el token de acceso está expirado y refrescarlo si es necesario
     if gauth.access_token_expired:
         gauth.Refresh()
-    
-    # Guardar las credenciales actualizadas
-    gauth.SaveCredentialsFile("mycreds.txt")
-    
+
+    # Guardar las credenciales actualizadas (opcional)
+    # gauth.SaveCredentialsFile("mycreds.txt")
+
     # Crear el objeto GoogleDrive con las credenciales configuradas
     drive = GoogleDrive(gauth)
     return drive
+
 
 def download_latest_google_doc(drive, folder_id, local_file_name):
     file_list = drive.ListFile({'q': f"'{folder_id}' in parents and trashed=false"}).GetList()
