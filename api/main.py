e8776b85c184fcac46f81b36d33096d2feb1dd48
GastonMora42 <gastonmora1742@gmail.com>
2024-04-12 10:59:46 -0300

    Agregamos feedback

--- api/main.py
+++ api/main.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
-import random
 import os
 
 app = FastAPI(docs_url="/")
@@ -28,7 +27,7 @@ embedding_openai = OpenAIEmbeddings(
 )
 NOMBRE_INDICE_CHROMA = "dataset-contactos"
 vectorstore_chroma = Chroma(persist_directory=NOMBRE_INDICE_CHROMA, embedding_function=embedding_openai)
-retriever_chroma = vectorstore_chroma.as_retriever(search_kwargs={"k": 1})
+retriever_chroma = vectorstore_chroma.as_retriever()
 llm = ChatOpenAI(
     model_name="gpt-3.5-turbo",
     openai_api_key=os.environ.get("OPENAI_API_KEY"),
@@ -37,17 +36,30 @@ llm = ChatOpenAI(
 qa_chains_with_sources = RetrievalQAWithSourcesChain.from_chain_type(
     llm=llm,
     chain_type="stuff",
-    retriever=retriever_chroma
+    retriever=retriever_chroma,
 )
 
 @app.post("/generate_text")
-async def generate_text(input_data: InputData):
+async def generate_text(input_data: InputData, use_chroma: bool = False, feedback: bool = False):
     try:
-        # Realizar consultas al modelo con un rango aleatorio
-        num_queries = random.randint(1, 5)
-        responses = [qa_chains_with_sources(input_data.text) for _ in range(num_queries)]
-        # Seleccionar aleatoriamente una respuesta de las generadas
-        random_response = random.choice(responses)
-        return {"generated_text": random_response}
+        # Si se ha seleccionado la opción para utilizar Chroma
+        if use_chroma:
+            qa_chains_with_sources.retriever = retriever_chroma
+
+        # Concatenar el input de texto con el string predeterminado
+        concatenated_text = input_data.text + ""
+
+        # Realizar una sola consulta al modelo utilizando el texto concatenado
+        response = qa_chains_with_sources(concatenated_text)
+
+        # Guardar la retroalimentación del usuario si se proporcionó
+        if feedback is not None:
+            # Aquí puedes realizar alguna acción con la retroalimentación, como guardarla en una base de datos o utilizarla para entrenar tu modelo
+            if feedback:  # Si la retroalimentación es positiva
+                print("La respuesta es buena.")
+            else:  # Si la retroalimentación es negativa
+                print("La respuesta no es buena.")
+
+        return {"generated_text": response}
     except Exception as e:
         return {"error": f"Error en la generación del texto: {e}"}
