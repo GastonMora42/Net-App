516255caeb2d519ca5c0bcc3515b867aebaffed6
GastonMora42 <gastonmora1742@gmail.com>
2024-04-12 09:05:09 -0300

    Probamos con el codigo de commit anterior

--- get_last_file.py
+++ get_last_file.py
@@ -4,14 +4,11 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from oauth2client.client import OAuth2Credentials
 from datetime import datetime
-
 def configure_google_drive():
     gauth = GoogleAuth()
-
     # Obtener las credenciales desde las variables de entorno
     credentials_json = os.getenv('MYCREDSGOOGLE')
     credentials_dict = json.loads(credentials_json)
-
     # Crear un objeto OAuth2Credentials desde el JSON
     credentials = OAuth2Credentials(
         credentials_dict['access_token'],
@@ -27,45 +24,32 @@ def configure_google_drive():
         credentials_dict['token_response'],
         credentials_dict['scopes']
     )
-
-    #Cambiamos los secrets json de github
-
     # Asignar las credenciales al objeto gauth
     gauth.credentials = credentials
-
     # Verificar si el token de acceso está expirado y refrescarlo si es necesario
     if is_token_expired(credentials_dict['token_expiry']):
         gauth.Refresh()
-
     # Crear el objeto GoogleDrive con las credenciales configuradas
     drive = GoogleDrive(gauth)
     return drive
-
 def is_token_expired(token_expiry_str):
-    # Convertir la cadena de texto a datetime
     token_expiry_datetime = datetime.strptime(token_expiry_str, "%Y-%m-%dT%H:%M:%SZ")
     now = datetime.utcnow()
     return now >= token_expiry_datetime
-
 def download_latest_google_doc(drive, folder_id, local_file_name):
     file_list = drive.ListFile({'q': f"'{folder_id}' in parents and trashed=false"}).GetList()
-
     google_doc_files = [file for file in file_list if file['mimeType'] == 'application/vnd.google-apps.document']
-
     if not google_doc_files:
         print("No se encontraron documentos de Google en la carpeta.")
         return
-
     latest_google_doc = max(google_doc_files, key=lambda x: x['modifiedDate'])
-
     print(f"Descargando el último documento de Google: {latest_google_doc['title']}, Fecha de modificación: {latest_google_doc['modifiedDate']}")
-
     latest_google_doc.GetContentFile(local_file_name, mimetype='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-
     print(f"El último documento se ha descargado exitosamente como {local_file_name}")
 
 def main():
     folder_id = '1T54m4fmnMr-GSznRhdT7YU3mhaCOWerB'
+    local_file_name = '/dataset/ultimo_documento.docx'
     local_file_name = 'dataset/ultimo_documento.docx'
 
     drive = configure_google_drive()
