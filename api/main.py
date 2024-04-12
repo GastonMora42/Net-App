cd4c75258c64ef038759c7bcded2b3dfe36533a7
GastonMora42 <gastonmora1742@gmail.com>
2024-04-12 09:27:23 -0300

    probamos otra modificacion en main.py

--- api/main.py
+++ api/main.py
@@ -21,42 +21,33 @@ app.add_middleware(
 class InputData(BaseModel):
     text: str
 
-# Definir una clase de dependencia para inicializar los objetos globales una vez
-class InitializeChain:
-    def __init__(self):
-        # Inicializar los objetos globales aquí
-        embedding_openai = OpenAIEmbeddings(
-            model="text-embedding-3-large",
-            openai_api_key=os.environ.get("OPENAI_API_KEY")
-        )
-        nombre_indice_chroma = "dataset-contactos"
-        vectorstore_chroma = Chroma(persist_directory=nombre_indice_chroma, embedding_function=embedding_openai)
-        retriever_chroma = vectorstore_chroma.as_retriever(search_kwargs={"k": 1})
-        llm = ChatOpenAI(
-            model_name="gpt-3.5-turbo",
-            openai_api_key=os.environ.get("OPENAI_API_KEY"),
-            temperature=1,
-        )
-        self.qa_chains_with_sources = RetrievalQAWithSourcesChain.from_chain_type(
-            llm=llm,
-            chain_type="stuff",
-            retriever=retriever_chroma
-        )
-
-    def get_chain(self):
-        return self.qa_chains_with_sources
-
-initialize_chain = InitializeChain()
+# Inicialización global de objetos
+embedding_openai = OpenAIEmbeddings(
+    model="text-embedding-3-large",
+    openai_api_key=os.environ.get("OPENAI_API_KEY")
+)
+NOMBRE_INDICE_CHROMA = "dataset-contactos"
+vectorstore_chroma = Chroma(persist_directory=NOMBRE_INDICE_CHROMA, embedding_function=embedding_openai)
+retriever_chroma = vectorstore_chroma.as_retriever(search_kwargs={"k": 1})
+llm = ChatOpenAI(
+    model_name="gpt-3.5-turbo",
+    openai_api_key=os.environ.get("OPENAI_API_KEY"),
+    temperature=1,
+)
+qa_chains_with_sources = RetrievalQAWithSourcesChain.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=retriever_chroma
+)
 
 @app.post("/generate_text")
-async def generate_text(input_data: InputData, chain=Depends(initialize_chain.get_chain)):
+async def generate_text(input_data: InputData):
     try:
         # Realizar consultas al modelo con un rango aleatorio
         num_queries = random.randint(1, 5)
-        responses = [chain(input_data.text) for _ in range(num_queries)]
+        responses = [qa_chains_with_sources(input_data.text) for _ in range(num_queries)]
         # Seleccionar aleatoriamente una respuesta de las generadas
         random_response = random.choice(responses)
         return {"generated_text": random_response}
     except Exception as e:
         return {"error": f"Error en la generación del texto: {e}"}
-
