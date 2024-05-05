8c59a94290815f8a77cdd980f349f2776cf7a7a0
GastonMora42 <gastonmora1742@gmail.com>
2024-05-04 21:08:36 -0300

    Funciona con el vector store en MongoDB, falta agregar memoria

--- api/main.py
+++ api/main.py
@@ -6,6 +6,14 @@ from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 import os
+from train_model import vector_search
+import pymongo
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain.prompts import PromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from pymongo import MongoClient
 
 app = FastAPI(docs_url="/")
 
@@ -29,32 +37,45 @@ embedding_openai = OpenAIEmbeddings(
     openai_api_key=os.environ.get("OPENAI_API_KEY")
 )
 
-# Inicializar la base de datos Chroma
-NOMBRE_INDICE_CHROMA = "dataset-contactos"
-vectorstore_chroma = Chroma(persist_directory=NOMBRE_INDICE_CHROMA, embedding_function=embedding_openai)
+# Instantiate Atlas Vector Search as a retriever
+retriever = vector_search.as_retriever(
+   search_type = "similarity",
+   search_kwargs = {"k": 15, "score_threshold": 0.75}
+)
 
 # Inicializar el modelo de ChatOpenAI
 llm = ChatOpenAI(
     model_name="gpt-4-turbo",
-    temperature=0.2,
+    temperature=4,
     max_tokens=1000,
 )
 
-# Inicializar la conversación
-conversation = ConversationalRetrievalChain.from_llm(
-    llm=llm, retriever=vectorstore_chroma.as_retriever(), verbose=True
-)
+# Define a prompt template
+template = """
+Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+{context}
+Question: {query}
+"""
+custom_rag_prompt = PromptTemplate.from_template(template)
 
-# Historial de la conversación
-conversation_history = []
+ # Historial de la conversación
+conversation_history = [],
+
+def format_docs(docs):
+   return "\n\n".join(doc.page_content for doc in docs)
+
 
 @app.post("/generate_text")
 async def generate_text(input_data: InputData):
-    try:
+    try:# Inicializar la conversación
         query = input_data.text
-        response = process_query(query)
-        # Actualizar el historial de la conversación
-        conversation_history.append((query, response))
+        rag_chain = (
+        { "context": retriever | format_docs, "query": RunnablePassthrough()}
+         | custom_rag_prompt
+         | llm
+         | StrOutputParser())
+        response = rag_chain.invoke(query)
         return {"generated_text": response}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error en la generación del texto: {e}")
@@ -67,15 +88,9 @@ async def send_feedback(feedback: Feedback):
             # Aquí puedes realizar alguna acción con la retroalimentación positiva
             prompt_feedback = "Buena respuesta"
         else:
-            print("La respuesta no es buena.")
+            print("La respuesta no es buenas.")
             # Aquí puedes realizar alguna acción con la retroalimentación negativa
             prompt_feedback = "Mala respuesta"
         return {"message": "Feedback recibido correctamente."}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error al procesar el feedback: {e}")
-
-def process_query(query):
-    global conversation_history
-    print("[La IA está pensando...]")
-    result = conversation({"question": query, "chat_history": conversation_history})
-    return result["answer"]
--- train_model.py
+++ train_model.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy
 import openai
 import langchain
-import retriever
-import sentence_transformers
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.embeddings import SentenceTransformerEmbeddings
 from langchain.schema import Document
@@ -12,89 +10,47 @@ from langchain.vectorstores import Chroma
 from datetime import datetime
 from io import StringIO
 from langchain.chains import RetrievalQAWithSourcesChain
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import random
 import sys
 from langchain.chat_models import ChatOpenAI
 import logging
 from sklearn.pipeline import Pipeline
-from prepare import contacts_data
 from langchain.embeddings import OpenAIEmbeddings
 import os
+from pymongo import MongoClient
+from langchain.vectorstores import MongoDBAtlasVectorSearch
 
-logging.basicConfig(
-    format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
-    level=logging.INFO,
-    datefmt='%H:%M:%S',
-    stream=sys.stderr
-)
-
-logger = logging.getLogger(__name__)
-
-logger.info("Cargando claves y secretos..")
+from langchain_community.document_loaders.mongodb import MongodbLoader
 
 api_key = os.environ.get("OPENAI_API_KEY")
 openai_api_key = os.environ.get("OPENAI_API_KEY")
-
-logger.info("Cargando modelo....")
-
-model = Pipeline([
-    ('embeddingopenai', OpenAIEmbeddings(
-        openai_api_key="OPENAI_API_KEY",
-        model_name="text-embedding-3-large"
-    )),
-    ('index', Chroma(
-        persist_directory='NOMBRE_INDICE_CHROMA',
-        embedding_function='embeddingopenai',
-    )),
-    ('retriever_chroma', Chroma(
-        persist_directory='NOMBRE_INDICE_CHROMA',
-        embedding_function='embeddingopenai',
-    ).as_retriever(
-        search_kwargs={"k": 4}
-    )),
-    ('llm', ChatOpenAI(
-        openai_api_key="OPENAI_API_KEY",
-        model_name="gpt-4-turbo",
-        temperature=0.5
-    )),
-])
 
 embeddingopenai = OpenAIEmbeddings(
     model="text-embedding-3-large"
 )
 
-logger.info("Cargando data resumida...")
+connection_string="mongodb+srv://gastonmora1742:jIhdEUoE9FWAcunB@jett-cluster.psm4rdx.mongodb.net/";
+db_name="index-contactos";
+collection_name="index";
+index_name="index_name"
+
+client = MongoClient(connection_string)
+collection = client[db_name][collection_name]
 
 loader = CSVLoader(file_path='dataset/Contacts-Main View.csv')
 data = loader.load()
 
-NOMBRE_INDICE_CHROMA = "dataset-contactos"
+collection.delete_many({});
 
-vectorstore_chroma = Chroma.from_documents(
-    documents=data,
-    embedding=embeddingopenai,
-    persist_directory=NOMBRE_INDICE_CHROMA
-)
+embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
-vectorstore_chroma.persist()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+docs = text_splitter.split_documents(data)
 
-vectorstore_chroma = Chroma(
-    persist_directory=NOMBRE_INDICE_CHROMA, 
-    embedding_function=embeddingopenai
-)
-
-retriever_chroma = vectorstore_chroma.as_retriever(
-    search_kwargs={"k" : 2}
-)
-
-llm = ChatOpenAI (
-    model_name="gpt-4-turbo",
-    openai_api_key="OPENAI_API_KEY",
-    temperature=1,
-)
-
-qa_chains_whith_sources = RetrievalQAWithSourcesChain.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=retriever_chroma
+vector_search = MongoDBAtlasVectorSearch.from_documents(
+    documents=docs,
+    embedding=OpenAIEmbeddings(disallowed_special=()),
+    collection=collection,
+    index_name=index_name,
 )
