7d2b587251c3bb15aea3ed606b62f342d5a285e8
GastonMora42 <gastonmora1742@gmail.com>
2024-05-06 23:51:52 -0300

    Insertamos

--- train_model.py
+++ train_model.py
@@ -1,43 +1,54 @@
 from dvc.api import read
 import pandas as pd
-import numpy as np
+import numpy
+import openai
+import langchain
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.schema import Document
+from langchain.vectorstores import Chroma
+from datetime import datetime
+from io import StringIO
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import random
+import sys
+from langchain.chat_models import ChatOpenAI
+import logging
+from sklearn.pipeline import Pipeline
+from langchain.embeddings import OpenAIEmbeddings
 import os
 from pymongo import MongoClient
 from langchain.vectorstores import MongoDBAtlasVectorSearch
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.schema import Document
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.document_loaders.csv_loader import CSVLoader
 
-# Configuración de la conexión a MongoDB
-connection_string = "mongodb+srv://gastonmora1742:jIhdEUoE9FWAcunB@jett-cluster.psm4rdx.mongodb.net/"
-db_name = "index-contactos"
-collection_name = "index"
-index_name = "index_name"
+from langchain_community.document_loaders.mongodb import MongodbLoader
 
-# Inicialización del cliente de MongoDB
+api_key = os.environ.get("OPENAI_API_KEY")
+openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+embeddingopenai = OpenAIEmbeddings(
+    model="text-embedding-3-large"
+)
+
+connection_string="mongodb+srv://gastonmora1742:jIhdEUoE9FWAcunB@jett-cluster.psm4rdx.mongodb.net/";
+db_name="index-contactos";
+collection_name="index";
+index_name="index_name"
+
 client = MongoClient(connection_string)
 collection = client[db_name][collection_name]
 
-# Cargar datos desde un archivo CSVs
 loader = CSVLoader(file_path='dataset/new-contact.csv')
 data = loader.load()
 
-# Inicializar embeddings de OpenAI
-openai_api_key = os.environ.get("OPENAI_API_KEY")
-embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key, disallowed_special=())
+embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
-# Dividir documentos en fragmentos (chunks)
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
 docs = text_splitter.split_documents(data)
 
-# Inicializar búsqueda de vectores en MongoDB
-vector_search = MongoDBAtlasVectorSearch(document=docs, embedding=embeddings, collection=collection, index_name=index_name)
-
-# Obtener el embedding del documento
-embedding = embeddings.embed(docs.page_content)
-
-# Almacenar el vector en MongoDB
-vector_search.store_vector(document=docs, embedding=embedding, collection=collection)
-
-print("Documentos y embeddings insertados exitosamente en MongoDB.")
+vector_search = MongoDBAtlasVectorSearch.from_documents(
+    documents=docs,
+    embedding=OpenAIEmbeddings(disallowed_special=()),
+    collection=collection,
+    index_name=index_name,
+)
