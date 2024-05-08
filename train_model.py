d7292b649f165066d09089e11a82a7691a88d031
GastonMora42 <gastonmora1742@gmail.com>
2024-05-08 00:25:54 -0300

    Pusheamos datos a DB de Netsquared

--- train_model.py
+++ train_model.py
@@ -6,6 +6,7 @@ from langchain.embeddings import OpenAIEmbeddings
 import os
 import pymongo
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 from langchain.vectorstores import MongoDBAtlasVectorSearch
 
 from langchain_community.document_loaders.mongodb import MongodbLoader
@@ -21,7 +22,7 @@ db_name="netsquared-db";
 collection_name="contactos";
 index_name="embbeding-contactos"
 
-client = MongoClient(connection_string)
+client = MongoClient(connection_string, server_api=ServerApi('1'))
 collection = client[db_name][collection_name]
 
 loader = CSVLoader(file_path='dataset/new-contact.csv')
@@ -32,7 +33,8 @@ embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
 docs = text_splitter.split_documents(data)
 
-vector_search = MongoDBAtlasVectorSearch.from_documents(
+vector_search = MongoDBAtlasVectorSearch.from_connection_string(
+    connection_string = connection_string,
     documents=docs,
     embedding=OpenAIEmbeddings(disallowed_special=()),
     collection=collection,
