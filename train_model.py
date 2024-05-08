090ec66eff7aa7a8e9a8bbff11628408185757a2
GastonMora42 <gastonmora1742@gmail.com>
2024-05-07 23:28:45 -0300

    Pusheamos datos a DB de Netsquared

--- train_model.py
+++ train_model.py
@@ -29,7 +29,7 @@ embeddingopenai = OpenAIEmbeddings(
     model="text-embedding-3-large"
 )
 
-connection_string="mongodb+srv://netsquared-db:L0PFljJA6c9bJNbV@netsquared-cluster.jmzk3jk.mongodb.net/";
+connection_string="mongodb+srv://netsquared:jalAcjL8zTQrDPMa@netsquared-cluster.jmzk3jk.mongodb.net/";
 db_name="netsquared-db";
 collection_name="contactos";
 index_name="embbeding-contactos"
